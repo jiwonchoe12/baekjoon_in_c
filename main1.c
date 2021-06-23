8424f8eca7e9ac5e6e0fd0eaa909e1a5a5e41467
최지원 <choejiwon@choejiwon-ui-MacBookAir.local>
2021-06-23 21:14:08 +0900

    6/23

---
+++ main1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int t;
+  scanf("%d", &t);
+
+  for (int i = 0; i<t; i++)
+  {
+    int a, b;
+    scanf("%d" "%d", &a, &b);
+    printf("%d", a+b);
+    printf("\n");
+  }
+  return 0;
+}
