286db60492817890c7427268d75343bc07532706
최지원 <belle021202@naver.com>
2021-08-14 23:00:44 +0900

    8/14

---
+++ 1002.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int t;
+  scanf("%d", &t);
+  for (int i=0; i<t; i++)
+  {
+    int x1, y1, r1, x2, y2, r2;
+    scanf("%d %d %d %d %d %d", &x1, &y1, &r1, &x2, &y2, &r2);
+    
+  }
+  return 0;
+}
