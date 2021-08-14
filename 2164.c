75ac6a37bd3cfe020719fcc3523e083b9bb2e9ab
최지원 <belle021202@naver.com>
2021-08-14 23:23:34 +0900

    8/14

---
+++ 2164.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int n;
+  int card[250000];
+  scanf("%d", &n);
+  if (n==1)
+    printf("%d", n);
+  else
+  {
+    if (n%2==0) //짝수
+    {
+      for (int i=2; i<n; i=i+2)
+      {
+
+      }
+    }
+    else //홀수
+    {
+      for (int i=2; i<n; i=i+2)
+      {
+
+      }
+    }
+  }
+
+  return 0;
+}
