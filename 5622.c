4b3c67454fe4a77ba8ecf97ae15df692bebac879
최지원 <belle021202@naver.com>
2021-08-18 23:31:00 +0900

    8/18

---
+++ 5622.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+  char a[16];
+  scanf("%s", a);
+  int count = 0;
+  char b[24]
+
+  for (int i=0; i<strlen(a); i++)
+  {
+    count = count + a[i]
+  }
+
+
+  printf("%d", strlen(a) + count);
+
+  return 0;
+}
