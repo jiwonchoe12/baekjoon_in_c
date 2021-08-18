270aac468d7e2d9b8f87e36933c3ec5c71215d2f
최지원 <belle021202@naver.com>
2021-08-18 23:37:56 +0900

    8/18

---
+++ 11650.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int n;
+  scanf("%d", &n);
+  int a[100000][2];
+
+  for (int i=0; i<n; i++)
+  {
+    scanf("%d %d", &a[i][0], &a[i][1]);
+  }
+  int min = a[0][0];
+  for (int i=0; i<n-1; i++)
+  {
+    for (int j=1; j<n; j++)
+    {
+      if ()
+      {
+
+      }
+      if ()
+      {
+
+      }
+    }
+  }
+
+
+
+  return 0;
+}
